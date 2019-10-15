bae4d38c8c7e64e3e4bff41ac445be75b27716a1
GabinTodesco <gabin.todesco@etu.unice.fr>
2019-10-15 15:51:37 +0100

    add structural tests

--- SoftwareEngineering/tests/TP1/DirectAcyclicGraphTest.java
+++ SoftwareEngineering/tests/TP1/DirectAcyclicGraphTest.java
@@ -9,7 +9,7 @@ public class DirectAcyclicGraphTest
 {
     DirectAcyclicGraph<Integer> dag;
     DAGNode<Integer>[] nodes;
-    boolean wellInstancied;
+    boolean wellInstanced;
 
     @BeforeEach
     void setUp()
@@ -22,24 +22,39 @@ public class DirectAcyclicGraphTest
             nodes[i] = new DAGNode<>(i+1);
         }
 
-        wellInstancied = (dag.putChild(nodes[0], nodes[1]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[0], nodes[2]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[1], nodes[3]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[3], nodes[5]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[2], nodes[4]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[4], nodes[6]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[4], nodes[7]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[6], nodes[9]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[9], nodes[8]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[9], nodes[12]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[9], nodes[10]) != null);
-        wellInstancied = wellInstancied && (dag.putChild(nodes[10], nodes[11]) != null);
+        wellInstanced = (dag.putChild(nodes[0], nodes[1]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[0], nodes[2]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[1], nodes[3]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[3], nodes[5]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[2], nodes[4]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[4], nodes[6]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[4], nodes[7]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[6], nodes[9]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[9], nodes[8]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[9], nodes[12]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[9], nodes[10]) != null);
+        wellInstanced = wellInstanced && (dag.putChild(nodes[10], nodes[11]) != null);
     }
 
     @Test
     void structural()
     {
-        assertTrue(wellInstancied);
+        // well instanced
+        assertTrue(wellInstanced);
+
+        //  well formed
+        assertTrue(nodes[0].getChildren().contains(nodes[1]));
+        assertTrue(nodes[0].getChildren().contains(nodes[2]));
+        assertTrue(nodes[1].getChildren().contains(nodes[3]));
+        assertTrue(nodes[3].getChildren().contains(nodes[5]));
+        assertTrue(nodes[2].getChildren().contains(nodes[4]));
+        assertTrue(nodes[4].getChildren().contains(nodes[6]));
+        assertTrue(nodes[4].getChildren().contains(nodes[7]));
+        assertTrue(nodes[6].getChildren().contains(nodes[9]));
+        assertTrue(nodes[9].getChildren().contains(nodes[8]));
+        assertTrue(nodes[9].getChildren().contains(nodes[12]));
+        assertTrue(nodes[9].getChildren().contains(nodes[10]));
+        assertTrue(nodes[10].getChildren().contains(nodes[11]));
     }
 
     @Test
